5af22e79f2de1f5bca8a51b6a9b82cdd11a7c4c4
Zane Brzezinski <Zane@joystickinteractive.com>
2016-09-15 11:21:58 -0400

    Who knows man

--- content.js
+++ content.js
@@ -1,8 +1,10 @@
 (function(){
+  console.log('running');
 
   var vertMid = $(window).height() / 2;
   var horzMid = $(window).width() / 2;
 
+  var colors = ['#000', '#fff', 'silver', 'red', 'grey'];
 
   var $head = $('head');
   var $body = $('body');
@@ -13,12 +15,7 @@
   $body.css('position', 'fixed');
   $body.css('left', horzMid);
   $body.css('top', vertMid);
-  $body.css("background-color", 'gainsboro');
-
-
-
-
-
+  $body.css("background-color", colors[getRandomInt(0,colors.length)]);
 
   var $all = $('*');
 
@@ -26,15 +23,16 @@
     return Math.floor(Math.random() * (max - min + 1)) + min;
   };
 
-  var colors = ['#f0ad00', 'black', 'silver', '#FFF', '$ffb800', '#d89b00'];
   var transform = function(){
     $all.map(function(){
       var $el = $(this);
-      if ($el.attr('class') != 'exempt') {
+      $el.css("background-color", colors[getRandomInt(0,colors.length)]);
+      if (!($el.hasClass('exempt'))) {
         $el.css("color", colors[getRandomInt(0,colors.length)]);
-        $el.css("background-color", colors[getRandomInt(0,colors.length)]);
+        $el.css("top", getRandomInt(0, 100));
+        $el.css("left", getRandomInt(0, 100));
         $el.css('font-size', getRandomInt(1,100));
-        $el.css('transition', 'transform ' + getRandomInt(0,5000) + 'ms, font-size ' + getRandomInt(0,5000) + 'ms');
+        $el.css('transition', 'transform ' + getRandomInt(0,5000) + 'ms, top '+ getRandomInt(0,5000) + 'ms, left '+ getRandomInt(0,5000) + 'ms');
         $el.css('position', 'fixed');
         var str = 'rotate3d('+getRandomInt(0,20)+','+getRandomInt(0,20)+','+getRandomInt(0,20)+','+getRandomInt(-90,90)+'deg)';
         $el.css('transform', str);
@@ -46,36 +44,37 @@
       }
     });
   }
-
-  var $pause = $('<i class="fa fa-pause exempt" id="pause-play" aria-hidden="true"></i>');
-  $pause.css('position', 'fixed');
-  $pause.css('color', 'black');
-  $pause.css('left', 0);
-  $pause.css('top', 0);
-  $pause.css('margin', '20px');
-  $pause.css('cursor', 'pointer');
-  $body.append($pause);
+  //
+  // var $pause = $('<i class="fa fa-pause exempt" id="pause-play" aria-hidden="true"></i>');
+  // $pause.css('position', 'fixed');
+  // $pause.css('color', 'black');
+  // $pause.css('left', 0);
+  // $pause.css('top', 0);
+  // $pause.css('margin', '20px');
+  // $pause.css('cursor', 'pointer');
+  // $body.append($pause);
 
   transform();
-  window.interval = setInterval(function(){
+  var interval = setInterval(function(){
     transform();
   }, 5000);
 
-  $pause.on('click', function(){
-    if (window.isRunning) {
-      clearInterval(window.interval);
-      window.isRunning = false;
-      console.log(window.isRunning);
-    } else {
-      transform();
-      window.interval = setInterval(function(){
-        transform();
-      }, 11000);
-      window.isRunning = true;
-      console.log(window.isRunning);
-    }
-  });
-
+  // $pause.on('click', function(){
+  //   console.log(window.isRunning);
+  //   if (window.isRunning) {
+  //     clearInterval(interval);
+  //     window.isRunning = false;
+  //     console.log(window.isRunning);
+  //   } else {
+  //     transform();
+  //     interval = setInterval(function(){
+  //       transform();
+  //     }, 11000);
+  //     window.isRunning = true;
+  //     console.log(window.isRunning);
+  //   }
+  // });
+  //
 
 
 
